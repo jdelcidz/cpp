38b2f5b2622ca3f8270d35b55aa0d118a6ccf41a
Jose Del Cid <98363558+jdelcidz@users.noreply.github.com>
2022-02-19 00:32:41 -0700

    Add files via upload

---
+++ Ejercicio 03 - Variables/variables.cpp
@@ -0,0 +1,19 @@
+/*Ejercicio numero 3, Variables */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroentero;
+    numeroentero = 15;
+    
+    cout << "Entero :" << numeroentero << endl;
+    
+    numeroentero = 18;
+    
+    cout << "Entero Modificado:" << numeroentero << endl;
+    
+    return 0;
+}
