eb0f526590d09129db14dfc62fcef9f7906a011a
Jose Del Cid <98363558+jdelcidz@users.noreply.github.com>
2022-02-19 00:53:37 -0700

    Add files via upload

---
+++ Ejercicio 16 - Ciclos While/ciclowhile.cpp
@@ -0,0 +1,19 @@
+//Programa ciclo while.
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char** argv){
+        
+    int a= 5;
+    int b= 2;
+
+    while (a > b ){
+        cout <<"\n Ingrese el valor de a:" ;
+        cin >> a;
+	}
+	
+	cout <<"\n Saliste del Ciclo!" ;
+	return 0;
+}
