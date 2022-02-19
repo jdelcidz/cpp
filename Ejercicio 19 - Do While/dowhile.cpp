9ebc201056c8574545effe027beace0d586fd780
Jose Del Cid <98363558+jdelcidz@users.noreply.github.com>
2022-02-19 02:15:55 -0700

    Add files via upload

---
+++ Ejercicio 19 - Do While/dowhile.cpp
@@ -0,0 +1,25 @@
+//Programa ciclo do while.
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char const *argv[]){
+     
+    int a= 2;
+    int b= 10;
+    
+        while (a > b){
+        cout << "(while) a > b " << endl;
+        break;
+        
+    }
+	
+	do {
+	     cout << "(do while) a > b " << endl;
+	     break;
+	     
+	} while (a > b);
+	
+	return 0;
+}
